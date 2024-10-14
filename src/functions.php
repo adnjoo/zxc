1f3c866153c70d5baa1cced8eb81f3fa8a424a54
Andrew Njoo <adnjoo@gmail.com>
2024-10-14 12:41:40 -0500

    h

--- src/functions.php
+++ src/functions.php
@@ -1,23 +1,20 @@
 <?php
-include 'db.php'; // Include the database connection logic
+// Check if the session is already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start(); // Start the session if it hasn't been started already
+}
+
+// Include the database connection logic
+include '../src/db.php'; 
 
 function addTodoItem($title) {
     // Get the database connection
     $dbconn = getDbConnection();
-
-    // Escape user input to prevent SQL injection
     $title = pg_escape_string($dbconn, $title);
-    
-    // Prepare the SQL query
     $query = "INSERT INTO todos (title) VALUES ('$title')";
     $result = pg_query($dbconn, $query);
-    
-    // Check if the query was successful
     if (!$result) {
         die("Error in SQL query: " . pg_last_error());
     }
-
-    // Close the database connection
     pg_close($dbconn);
 }
-?>
